055807136e00b3e595fd4d8bc059b8df9ffbfe55
Paul <mr_paul_mayer@gmx.de>
2023-05-12 19:25:42 +0200

    Testing

--- ml_test.py
+++ ml_test.py
@@ -21,8 +21,10 @@ def get_embeddings(embedder, corpus):
 
 def cross_validation(corpus_per_inform):
     embedder = SentenceTransformer('PM-AI/bi-encoder_msmarco_bert-base_german')
+    #embedder = SentenceTransformer('aari1995/German_Semantic_STS_V2', device='cuda:0')
+    scores = []
     for inform in corpus_per_inform:
-        scores = []
+        #scores = []
         for sentence in inform[1]:
             corpus_test = inform[1].copy()
             corpus_test.remove(sentence)
@@ -34,14 +36,38 @@ def cross_validation(corpus_per_inform):
             cos_scores = util.cos_sim(query_embedding, corpus_embeddings)[0]
             top_results = torch.topk(cos_scores, k=1)
             scores.append(top_results[0][0].item())
-        print("inform: ", inform[0])
-        print("scores: ", scores)
-        print("mean: ", sum(scores)/len(scores))
-        print("max: ", max(scores))
-        print("min: ", min(scores))
-        print("\n====================================================\n")
+        #print("inform: ", inform[0])
+    print("scores: ", scores)
+    print("mean: ", sum(scores)/len(scores))
+    print("max: ", max(scores))
+    print("min: ", min(scores))
+    print("\n====================================================\n")
         
 
+def cross_validation_across_informs(corpus_per_inform, corpus_full):
+    embedder = SentenceTransformer('PM-AI/bi-encoder_msmarco_bert-base_german', device='cuda:0')
+    #embedder = SentenceTransformer('aari1995/German_Semantic_STS_V2', device='cuda:0')
+    scores = []
+    for inform in corpus_per_inform:
+        #scores = []
+        corpus_test = corpus_full.copy()
+        for sentence in inform[1]:
+            corpus_test.remove(sentence)
+        print("corpus_test: ", len(corpus_test))
+        corpus_embeddings = get_embeddings(embedder, corpus_test)
+        for sentence in inform[1]:
+            query_embedding = embedder.encode(sentence, convert_to_tensor=True)
+      
+            # We use cosine-similarity and torch.topk to find the highest 5 scores
+            cos_scores = util.cos_sim(query_embedding, corpus_embeddings)[0]
+            top_results = torch.topk(cos_scores, k=1)
+            scores.append(top_results[0][0].item())
+        
+    print("scores: ", scores)
+    print("mean: ", sum(scores)/len(scores))
+    print("max: ", max(scores))
+    print("min: ", min(scores))
+    print("\n====================================================\n")
 
 
 
@@ -73,7 +99,7 @@ def manual(embedder, corpus_train, train_labels, corpus_embeddings = None):
         
         print("\n\n======================\n\n")
         print("Query:", query)
-        print("\nTop 5 most similar sentences in corpus in ", time_end-time_start, "seconds:")
+        print("\nTop 5 most similar sentences in corpus in ", time_end-time_start, "seconds, and on device: ", query_embedding.device)
 
         for score, idx in zip(top_results[0], top_results[1]):
             print(corpus_train[idx], "(Score: {:.4f})".format(score))
@@ -110,21 +136,30 @@ def automatic(embedder, corpus_train, train_labels, corpus_test, test_labels, co
 def main():
     corpus = Corpus()
     while(True):
+        print(torch.cuda.is_available())
+        print(torch.__version__)
         print("---------------------------")
-        print("Do you want to... \n 1.make one csv \n 2.load corpus with strasfied sampling \n 3. Cross Validation \n 4.load the full corpus \n 5.load cached embeddings \n 6.exit")
+        print("Do you want to... \n 1.make one csv \n 2.load corpus with strasfied sampling \n 3. Cross Validation \n 4.Cross Val across informs \n 5.load the full corpus \n 6.load cached embeddings \n 7.exit")
         choice = input()
         
         match choice:
             case '1':
-                corpus.make_csv('./big_corpus/', 'big_corpus')
+                corpus.make_csv('./corpus/', 'corpus')
                 print("Done making csv")
                 continue
             case '2':
-                corpus_train, train_labels, corpus_test, test_labels = corpus.load_stratisfied('./big_corpus/', 'stratisfied')
+                corpus_train, train_labels, corpus_test, test_labels = corpus.load_stratisfied('./big_corpus/', 'stratisfied')              
+                corpus_embeddings = None
             case '3':
-                corpus = corpus.load_corpus_per_inform('./big_corpus/')
-                cross_validation(corpus)
+                corpus_informs = corpus.load_corpus_per_inform('./corpus/')
+                cross_validation(corpus_informs)
             case '4':
+                corpus_informs = corpus.load_corpus_per_inform('./corpus/')
+                corpus_train, _ = corpus.load_corpus('corpus_train.csv')
+                corpus_test, _ = corpus.load_corpus('corpus_test.csv')
+                corpus_full = corpus_train + corpus_test
+                cross_validation_across_informs(corpus_informs, corpus_full)
+            case '5':
                 corpus_train, train_labels = corpus.load_corpus('big_corpus_train.csv')
                 corpus_test, test_labels = corpus.load_corpus('big_corpus_test.csv')
                 corpus_embeddings = None
@@ -133,14 +168,14 @@ def main():
                 print("train_labels: ", len(train_labels))
                 print("test_labels: ", len(test_labels))
 
-            case '5':
+            case '6':
                 with open('embeddings.pkl', "rb") as fIn:
                     stored_data = pickle.load(fIn)
                     corpus_train = stored_data['sentences']
                     corpus_embeddings = stored_data['embeddings']
                     train_labels = stored_data['labels']
                 corpus_test, test_labels = corpus.load_corpus('big_corpus_test.csv')
-            case '6':
+            case '7':
                 exit()
             case _:
                 print("Invalid input")
@@ -150,15 +185,15 @@ def main():
         print("loading model... \n")
         
         #embedder = SentenceTransformer('distiluse-base-multilingual-cased-v2') # 539 MB
-        #embedder = SentenceTransformer('all-MiniLM-L6-v2') # 90.9 MB
+        embedder = SentenceTransformer('all-MiniLM-L6-v2', device='cuda') # 90.9 MB
         #embedder = SentenceTransformer('distilbert-multilingual-nli-stsb-quora-ranking') # 539 MB
         #embedder = SentenceTransformer('symanto/sn-xlm-roberta-base-snli-mnli-anli-xnli') # 1.11 GB
-        embedder = SentenceTransformer('aari1995/German_Semantic_STS_V2') # 1.34 GB
+        #embedder = SentenceTransformer('aari1995/German_Semantic_STS_V2', device='cuda') # 1.34 GB
         #embedder = SentenceTransformer('bert-base-multilingual-uncased') # 672 MB
         #embedder = SentenceTransformer('deutsche-telekom/gbert-large-paraphrase-cosine')
         #embedder = SentenceTransformer('Sahajtomar/German-semantic')
         #embedder = SentenceTransformer('clips/mfaq')
-        #embedder = SentenceTransformer('PM-AI/bi-encoder_msmarco_bert-base_german')
+        #embedder = SentenceTransformer('PM-AI/bi-encoder_msmarco_bert-base_german', device='cuda')
         #embedder = SentenceTransformer('PM-AI/sts_paraphrase_xlm-roberta-base_de-en')
         #embedder = SentenceTransformer('nblokker/debatenet-2-cat')
         print("Number parameter: ", sum(p.numel() for p in embedder.parameters()))
