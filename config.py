b7c57bea535c82010672894cde6d98f894068cdd
sheogorath15 <48446789+sheogorath15@users.noreply.github.com>
2023-08-16 12:59:22 +0200

    Update config.py to load environment variables from docker config

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
+import os
 
+MOODLE_SERVER_WEB_HOST = os.environ["MOODLE_SERVER_IP_ADDR"] # IP Adress of moodle webserver 
+MOODLE_SERVER_WEB_PORT = os.environ['MOODLE_SERVER_PORT']    # Port of moodle webserver (default: 8080, or set to None)
 
-MOODLE_SERVER_IP_ADDR = "localhost:80/moodle"    # IP address of moodle server
-MOODLE_SERVER_PORT = 8000    # Port of moodle webserver (default: 8080, or set to None)
+MOODLE_SERVER_IP_ADDR = f"{MOODLE_SERVER_WEB_HOST}:{MOODLE_SERVER_WEB_PORT}/moodle" # base adress of moodle website
 MOODLE_SERVER_DB_ADDR = "db"  # IP address of moodle database (e.g. same as web server IP if running on the same server)
-MOODLE_SERVER_DB_PORT = "3306"  # Port of moodle database (default for mysql: 3306)
+MOODLE_SERVER_DB_PORT = os.environ['MOODLE_DOCKER_DB_PORT']  # Port of moodle database (default for mysql: 3306)
 MOODLE_SERVER_DB_TALBE_PREFIX = "mdl_" # Prefix in Moodle Database Tables (e.g. m_, mdl_, ...)
 MOODLE_WSTOKEN = "13c3113a17b0d45a0016aec1216eaf34" # Token for Moodle web services
 
