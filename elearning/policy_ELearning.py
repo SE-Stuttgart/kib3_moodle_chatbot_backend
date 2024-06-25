e220b75147d5a0b0e5c6da31eb8572ed62edcde0
Dirk Väth <dirk.vaeth@ims.uni-stuttgart.de>
2024-06-25 14:06:35 +0000

    fix user id for webservice call (bookserach) with new block code

--- elearning/policy_ELearning.py
+++ elearning/policy_ELearning.py
@@ -493,7 +493,7 @@ class ELearningPolicy(Service):
             List of search results
             Boolean: if there are more search results
         """
-        book_links, has_more_results = get_book_links(webserviceuserid=self.get_webservice_user_id(user_id), wstoken=self.get_state(user_id, 'BOOKSEARCHTOKEN'), course_id=courseid, searchTerm=search_term, word_context_length=5, start=search_idx, end=search_idx+num_results)
+        book_links, has_more_results = get_book_links(webserviceuserid=user_id, wstoken=self.get_state(user_id, 'BOOKSEARCHTOKEN'), course_id=courseid, searchTerm=search_term, word_context_length=5, start=search_idx, end=search_idx+num_results)
         
         self.set_state(user_id=user_id, attribute_name=LAST_SEARCH_INDEX, attribute_value=search_idx + num_results)
         self.set_state(user_id=user_id, attribute_name=LAST_SEARCH, attribute_value=search_term)
